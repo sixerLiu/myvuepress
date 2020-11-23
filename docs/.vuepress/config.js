872ce6703f0cdab862e84102e8cc050a43bf3b1f
东酱 <sixerliu@outlook.com>
2020-11-23 23:33:35 +0800

    :tada: Initial commit.

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  base: '/myvuepress/',
+  head: [
+    ['link', { rel: 'icon', href: '/6.png' }]
+  ]
+}
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+# 啦啦啦啦啦
+
+test
+
+test
+
+1. test1
+2. test2
